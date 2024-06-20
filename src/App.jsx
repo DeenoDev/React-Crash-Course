4a6fa6e4974ad166269671d4b52e6f8621a65edb
DeenoDev <deyanoklaasen@outlook.com>
2024-06-20 23:16:33 +0200

    Embedded Navbar component into App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
+    
+    <Navbar />
 
 <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
