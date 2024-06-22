8bc9c7a32a384219f7e05353f1154c03cf206bcf
DeenoDev <deyanoklaasen@outlook.com>
2024-06-22 23:59:50 +0200

    imported Hero.jsx component into app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
@@ -7,22 +8,7 @@ const App = () => {
     
     <Navbar />
     
-    <section className="bg-indigo-700 py-20 mb-4">
-      <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-      >
-        <div className="text-center">
-          <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-          >
-            Become a React Dev
-          </h1>
-          <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
-          </p>
-        </div>
-      </div>
-    </section>
+    
 
     
     <section className="py-4">
