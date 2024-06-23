fc057590e53ccaa1b75b0430f12474ee5270abe8
DeenoDev <deyanoklaasen@outlook.com>
2024-06-23 23:51:23 +0200

    Used the destructured method to embed props.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Hero = (props) => {
+const Hero = ({title, subtitle}) => {
   return (
     <section className="bg-indigo-700 py-20 mb-4">
       <div
@@ -10,10 +10,10 @@ const Hero = (props) => {
           <h1
             className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
           >
-            {props.title}
+            {title}
           </h1>
           <p className="my-4 text-xl text-white">
-            {props.subtitle}
+            {subtitle}
           </p>
         </div>
       </div>
