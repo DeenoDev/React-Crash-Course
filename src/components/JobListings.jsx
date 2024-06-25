5014c9516d458819242e872200bab0efe1671945
DeenoDev <deyanoklaasen@outlook.com>
2024-06-25 23:51:56 +0200

    Used map to display joblisting cards and deleted hardcoded joblistings markup.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,8 +9,8 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          
-          <div className="bg-white rounded-xl shadow-md relative">
+          { jobs.map((job) =>(
+            <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Full-Time</div>
@@ -39,66 +39,14 @@ const JobListings = () => {
               </div>
             </div>
           </div>
+            
+          ) )}
           
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
-              </div>
-
-              <div className="mb-5">
-               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Miami, FL
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
           
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">React.js Developer</h3>
-              </div>
-
-              <div className="mb-5">
-                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Brooklyn, NY
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
+          
+         
+          
+          
         </div>
       </div>
     </section>
