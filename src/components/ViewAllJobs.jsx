8c9a768c6e273acdcbe7aea54814bf7e25fc0cb5
DeenoDev <deyanoklaasen@outlook.com>
2024-06-26 23:51:04 +0200

    ViewAllJobs markup completed.

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ViewAllJobs = () => {
   return (
-    <div>ViewAllJobs</div>
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
   )
 }
 
