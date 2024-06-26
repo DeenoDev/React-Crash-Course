7d6b773070c4174c8cd4af2c6a37b880d5babd4e
DeenoDev <deyanoklaasen@outlook.com>
2024-06-26 23:47:36 +0200

    created recentJobs variable by slicing the jobs variale.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,6 +3,8 @@ import jobs from '../jobs.json';
 import JobListing from './JobListing';
 
 const JobListings = () => {
+  const recentJobs = jobs.slice(0, 3);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,8 +12,8 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          { jobs.map((job) =>(
-            <JobListing job={ job }/>
+          { recentJobs.map((job) =>(
+            <JobListing key={ jobs.id } job={ job }/>
            
             
           ) )}
