2c26beccc2a502730729f93d95521c544119990a
DeenoDev <deyanoklaasen@outlook.com>
2024-06-26 23:56:20 +0200

    imported ViewAllJobs.jsx component into App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';;
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
   return (
@@ -15,13 +16,7 @@ const App = () => {
     
     
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    
 
     </>
   );
