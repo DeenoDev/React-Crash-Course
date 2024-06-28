36674ea1ed7a1e844fe19dc6d18ed3c88a5443b0
DeenoDev <deyanoklaasen@outlook.com>
2024-06-28 23:27:44 +0200

    replaced job.description with description variable.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -18,7 +18,7 @@ const JobListing = ({ job }) => {
       </div>
 
       <div className="mb-5">
-       { job.description }
+       { description }
       </div>
 
       <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
