577e1bf96e873cc6eea42e385c35cec9e345c40a
DeenoDev <deyanoklaasen@outlook.com>
2024-06-29 00:01:35 +0200

    embedded imported icon into JobListing.jsx file.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -30,7 +30,7 @@ const JobListing = ({ job }) => {
 
       <div className="flex flex-col lg:flex-row justify-between mb-4">
         <div className="text-orange-700 mb-3">
-          <i className="fa-solid fa-location-dot text-lg"></i>
+          <FaMapMarker />
           {job.location}
         </div>
         <a
