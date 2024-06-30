4d1c3253c5d0c37caa181d497fd54ef7d3597b58
DeenoDev <deyanoklaasen@outlook.com>
2024-06-30 23:30:33 +0200

    removed component imports from App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';;
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
