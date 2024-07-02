7553f83646987e0a6a1e454a85f1653227f9bb6e
DeenoDev <deyanoklaasen@outlook.com>
2024-07-02 22:51:12 +0200

    Fixed HomeCard bug due to syntax error in Card component which was embedded into parent HomeCard component.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import HomeCards from '../components/HomeCards';
-import JobListings from '../components/JobListing';
+
+import ViewAllJobs from '../components/ViewAllJobs';
 
 const HomePage = () => {
   return (
     <>
-    <Hero />
-    <HomeCards />
+      <Hero />
+      <HomeCards />
     </>
-  )
-}
-
+  );
+};
 export default HomePage;
