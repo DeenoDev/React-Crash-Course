bb2a2d8fb9ce31c6cf3a5a3f231fe1aac7887d3f
DeenoDev <deyanoklaasen@outlook.com>
2024-07-04 22:28:05 +0200

    changed all a tags to Link tags.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" href="/index.html">
               <img
                 className="h-10 w-auto"
                 src={ logo }
@@ -21,7 +21,7 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
+            </Link>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
@@ -29,11 +29,11 @@ const Navbar = () => {
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Home</a
                 >
-                <a
+                <Link
                   href="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
+                  >Jobs
+                  </Link>
                 <a
                   href="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
