462ea1eb322502545ab573ff0f1bb389a215d298
DeenoDev <deyanoklaasen@outlook.com>
2024-07-04 22:08:21 +0200

    embedded JobsPages into App.jsx routes.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element = {<MainLayout />}>
     <Route index element={<HomePage/>} />
+    <Route path='/jobs' element={<HomePage/>} />
   </Route>
   )
 );
