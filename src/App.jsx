12ada96d2ac4e62b17d5150c6c4fb4fd7cb04471
DeenoDev <deyanoklaasen@outlook.com>
2024-07-04 22:15:15 +0200

    Changes nav links within Navbar.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} f
 import React from 'react';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
-import JobsPages from './pages/JobsPages';
+import JobsPage from './pages/JobsPage';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element = {<MainLayout />}>
     <Route index element={<HomePage/>} />
-    <Route path='/jobs' element={<HomePage/>} />
+    <Route path='/jobs' element={<JobsPage/>} />
   </Route>
   )
 );
