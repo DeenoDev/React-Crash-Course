335db93e56b9743787743cebc933a0afa2178e72
DeenoDev <deyanoklaasen@outlook.com>
2024-07-04 22:33:19 +0200

    changed remaining link tags and href properties.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             
-            <Link className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" href="/">
               <img
                 className="h-10 w-auto"
                 src={ logo }
@@ -34,11 +34,11 @@ const Navbar = () => {
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Jobs
                   </Link>
-                <a
+                <Link
                   to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
+                  >Add Job
+                  </Link>
               </div>
             </div>
           </div>
