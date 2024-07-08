1e3232484eda0f62aed725f11475f7ca7d3a66c1
DeenoDev <deyanoklaasen@outlook.com>
2024-07-08 23:59:52 +0200

    Changed a tag to link tag.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,11 +7,11 @@ const NotFoundPage = () => {
       <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
-      <a
+      <Link
         href="/index.html"
         className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
         >Go Back
-        </a>
+        </Link>
     </section>
   )
 }
