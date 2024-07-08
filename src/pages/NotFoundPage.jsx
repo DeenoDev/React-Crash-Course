8c4179fc6b0d2490d70563de4838664edb755ae2
DeenoDev <deyanoklaasen@outlook.com>
2024-07-09 00:01:20 +0200

    Changed href into to tag.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
       <Link
-        href="/index.html"
+        to="/"
         className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
         >Go Back
         </Link>
