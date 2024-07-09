f766d0a4d5ec7f7f457009f374e5fff951564ae3
DeenoDev <deyanoklaasen@outlook.com>
2024-07-09 23:58:51 +0200

    Embedded NotFOundPage into Routes with the * symbol for path which means the 404 page will display on all not found instances.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const router = createBrowserRouter(
   <Route path='/' element = {<MainLayout />}>
     <Route index element={<HomePage/>} />
     <Route path='/jobs' element={<JobsPage/>} />
+    <Route path='*' element={<NotFoundPage/>} />
   </Route>
   )
 );
