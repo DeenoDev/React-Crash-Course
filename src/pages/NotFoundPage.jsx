e6d4921bb1f0a671b5898d7938c5c6d73ddde5f0
DeenoDev <deyanoklaasen@outlook.com>
2024-07-09 23:54:40 +0200

    modified styling class for icon.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,7 +5,7 @@ import { FaExclamationTriangle } from 'react-icons/fa';
 const NotFoundPage = () => {
   return (
     <section className="text-center flex flex-col justify-center items-center h-96">
-      <FaExclamationTriangle  className='text-yellow-400 fa-4x mb-4'/>
+      <FaExclamationTriangle  className='text-yellow-400 text-6xl mb-4'/>
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
       <Link
