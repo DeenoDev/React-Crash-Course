098deec1dc21d9d49afdd356fd21a69dc23f7479
DeenoDev <deyanoklaasen@outlook.com>
2024-07-11 00:02:20 +0200

    set isHome prop for JobListings.jsx component to true in HomePage.jsx file.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import jobs from '../jobs.json';
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false }) => {
-  const JobListings = isHome ? job.slice(0, 3) : jobs;
+  const JobListings = isHome ? jobs.slice(0, 3) : jobs;
 
   return (
     <section className="bg-blue-50 px-4 py-10">
