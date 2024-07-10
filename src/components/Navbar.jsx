07251d4362a569235768758e05275ffbc4bd31e4
DeenoDev <deyanoklaasen@outlook.com>
2024-07-10 23:34:37 +0200

    Changed all instances of Link to NavLink.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             
-            <Link className="flex flex-shrink-0 items-center mr-4" href="/">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" href="/">
               <img
                 className="h-10 w-auto"
                 src={ logo }
@@ -21,24 +21,24 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </Link>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <Link
+                <NavLink
                   to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Home
-                  </Link>
-                <Link
+                  </NavLink>
+                <NavLink
                   to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Jobs
-                  </Link>
-                <Link
+                  </NavLink>
+                <NavLink
                   to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Add Job
-                  </Link>
+                  </NavLink>
               </div>
             </div>
           </div>
