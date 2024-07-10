a8d1c344da1dbf31a4eb2cc32a07c96dcf300783
DeenoDev <deyanoklaasen@outlook.com>
2024-07-10 23:40:48 +0200

    Created linkClass function that will handle the state of NavBar links.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import logo from '../assets/images/logo.png';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+   const linkClass = ({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <>
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -26,7 +28,7 @@ const Navbar = () => {
               <div className="flex space-x-2">
                 <NavLink
                   to="/"
-                  className={ ({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'  }
+                  className={   }
                   >Home
                   </NavLink>
                 <NavLink
