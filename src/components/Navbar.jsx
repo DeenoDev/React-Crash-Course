31058485c7b347e5968ec0f1c4db86ab22dc3b29
DeenoDev <deyanoklaasen@outlook.com>
2024-07-10 23:32:45 +0200

    Imported NavLink from react-router-dom.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../assets/images/logo.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
