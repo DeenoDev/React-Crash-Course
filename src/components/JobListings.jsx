85ae843306a6f3d6590ec90f46f65eca680d5601
DeenoDev <deyanoklaasen@outlook.com>
2024-07-12 23:51:48 +0200

    turned jobs.json file into an object by using {} brackets.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = ({ isHome = false }) => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          { isHome ? 'Recent Jobs' : 'Browse Jobs'}
+          { isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           { JobListings.map((job) =>(
