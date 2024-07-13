00d9d3834b758bdaf00755dbb07a4e240680a38c
DeenoDev <deyanoklaasen@outlook.com>
2024-07-13 23:48:32 +0200

    imported useEffect and useState from react inside JobListings.jsx file.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 
+import React from 'react';
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false }) => {
