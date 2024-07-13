09c68a32b75782f12b545a651f0c8482d32e2656
DeenoDev <deyanoklaasen@outlook.com>
2024-07-13 23:58:33 +0200

    started scripting useEffect with async try catch function in JObListings.jsx component.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -7,6 +7,20 @@ const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs]= useState([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);  
+      } catch (error) {
+        
+      }
+       
+    }
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
