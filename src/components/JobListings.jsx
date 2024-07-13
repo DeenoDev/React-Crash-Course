79631f19c22b15dcde1787a82e5daf009af1a6a9
DeenoDev <deyanoklaasen@outlook.com>
2024-07-13 23:51:41 +0200

    created jobs and setJobs state in JobListings.jsx file.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false }) => {
-  
+  const [jobs, setJobs]= useState([]);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
