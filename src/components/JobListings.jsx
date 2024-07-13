01cd947341b678a0365a7889da13960e0fc90d06
DeenoDev <deyanoklaasen@outlook.com>
2024-07-14 00:00:19 +0200

    foundation of useEffect function completed.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -14,6 +14,7 @@ const JobListings = ({ isHome = false }) => {
         const data = await res.json();
         setJobs(data);  
       } catch (error) {
+        console.log('Error fetching data', error);
         
       }
        
