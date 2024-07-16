3d91729dc9fd9ac759b72c76d70dbebf56ab7886
DeenoDev <deyanoklaasen@outlook.com>
2024-07-16 23:49:55 +0200

    Used if else turnerary function to determine the amount of jobs that display on the home page.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,6 +10,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3': 'http://localhost:8000/jobs';
       try {
         const res = await fetch('http://localhost:8000/jobs');
         const data = await res.json();
