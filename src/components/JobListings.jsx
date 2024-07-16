edd596a2231a5a4afed0fd7a3e5306cb90b2076f
DeenoDev <deyanoklaasen@outlook.com>
2024-07-16 23:45:40 +0200

    refactored grid to allow spinner to be positioned in the middle.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -31,18 +31,18 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           { isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
           { loading ? (
             <Spinner />
           ) : (
-            <>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             { jobs.map((job) =>(
             <JobListing key={ job.id } job={ job }/>
              ) )} 
-            </>
+            </div>
           )}
            
-        </div>
+        
       </div>
     </section>
   )
