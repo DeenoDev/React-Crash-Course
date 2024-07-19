96b8825c2a854257f545954fcf66d536ea644cdc
DeenoDev <deyanoklaasen@outlook.com>
2024-07-20 00:00:32 +0200

    pasted the try catch template into useEffect function.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -5,7 +5,21 @@ const JobPage = () => {
   const [job, setJob] = useState(null);
 
   useEffect(() => {
-    
+    const fetchJob = async => {
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);  
+      } catch (error) {
+        console.log('Error fetching data', error);
+        
+      } finally{
+        setLoading(false);
+      }
+
+    }
+
+    fetchJob();
   },[])
 
 
