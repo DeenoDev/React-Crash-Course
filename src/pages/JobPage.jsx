cbae4db81dfa8e23e9667fa871a759c665a8a909
DeenoDev <deyanoklaasen@outlook.com>
2024-07-24 23:50:41 +0200

    imported Spinner component into JobPage.jsx file.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import Spinner from '../components/Spinner';
 
 const JobPage = () => {
   const { id } = useParams();
