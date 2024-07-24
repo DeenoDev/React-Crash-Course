65fd4d3b8d8efc61c7ad2ae5f52a09386f0a7e12
DeenoDev <deyanoklaasen@outlook.com>
2024-07-24 23:58:53 +0200

    commented out useState and useEffect import.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import Spinner from '../components/Spinner';
 
