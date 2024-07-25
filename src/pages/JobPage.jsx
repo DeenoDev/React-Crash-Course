475628c0cc63357141bf10513c1995b20d1ace2f
DeenoDev <deyanoklaasen@outlook.com>
2024-07-25 10:16:43 +0200

    Deleted all commented code for JobPage function.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -6,30 +6,8 @@ import Spinner from '../components/Spinner';
 const JobPage = () => {
   const { id } = useParams();
   const job = useLoaderData();
-  // const [job, setJob] = useState(null);
-  // const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const fetchJob = async () => {
-  //     try {
-  //       const res = await fetch(`/api/jobs/${id}`);
-  //       const data = await res.json();
-  //       setJob(data);  
-  //     } catch (error) {
-  //       console.log('Error fetching data', error);
-        
-  //     } finally{
-  //       setLoading(false);
-  //     }
-
-  //   }
-
-  //   fetchJob();
-  // },[])
-
-
-  return 
-    loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return  <h1>{job.title}</h1>;
 };
 
 const jobLoader = async ({ params }) => {
