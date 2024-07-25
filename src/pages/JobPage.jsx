5454c7af8cc95091f76f57532886534dc324afdc
DeenoDev <deyanoklaasen@outlook.com>
2024-07-25 22:27:25 +0200

    Added link tag to edit button.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -84,11 +84,10 @@ const JobPage = () => {
            
             <div class="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-              <a
+              <Link
                 href="/add-job.html"
                 class="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a
-              >
+                >Edit Job</Link>
               <button
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
