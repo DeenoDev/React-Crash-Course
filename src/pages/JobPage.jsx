6a7ff6ff27bc5932c810287a537008bb34c5fce0
DeenoDev <deyanoklaasen@outlook.com>
2024-07-25 22:28:57 +0200

    created dynamic link for edit button with template literals.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -85,7 +85,7 @@ const JobPage = () => {
             <div class="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 class="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                href="/add-job.html"
+                to={`/jobs/edit/${job.id}`}
                 class="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
               <button
