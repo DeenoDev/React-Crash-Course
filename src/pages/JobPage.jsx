08d12188df684a7436432931d45d80059eb7b689
DeenoDev <deyanoklaasen@outlook.com>
2024-07-25 10:24:51 +0200

    Changed all class properties to className.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -10,49 +10,49 @@ const JobPage = () => {
   return  (
     <>
       <section>
-      <div class="container m-auto py-6 px-6">
+      <div className="container m-auto py-6 px-6">
         <a
           href="/jobs.html"
-          class="text-indigo-500 hover:text-indigo-600 flex items-center"
+          className="text-indigo-500 hover:text-indigo-600 flex items-center"
         >
-          <i class="fas fa-arrow-left mr-2"></i> Back to Job Listings
+          <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
         </a>
       </div>
     </section>
 
-    <section class="bg-indigo-50">
-      <div class="container m-auto py-10 px-6">
-        <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+    <section className="bg-indigo-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
           <main>
             <div
-              class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+              className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
             >
-              <div class="text-gray-500 mb-4">Full-Time</div>
-              <h1 class="text-3xl font-bold mb-4">
+              <div className="text-gray-500 mb-4">Full-Time</div>
+              <h1 className="text-3xl font-bold mb-4">
                 Senior React Developer
               </h1>
               <div
-                class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+                className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
                 <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                  className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
                 ></i>
-                <p class="text-orange-700">Boston, MA</p>
+                <p className="text-orange-700">Boston, MA</p>
               </div>
             </div>
 
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 class="text-indigo-800 text-lg font-bold mb-6">
+            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+              <h3 className="text-indigo-800 text-lg font-bold mb-6">
                 Job Description
               </h3>
 
-              <p class="mb-4">
+              <p className="mb-4">
                We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
               </p>
 
-              <h3 class="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+              <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-              <p class="mb-4">$70k - $80K / Year</p>
+              <p className="mb-4">$70k - $80K / Year</p>
             </div>
           </main>
 
