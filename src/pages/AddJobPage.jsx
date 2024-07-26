53eeb90351f632ad3d9c0ba54623d97d3f51dfc9
DeenoDev <deyanoklaasen@outlook.com>
2024-07-27 00:01:00 +0200

    changed all instances of class to className.

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const AddJobPage = () => {
   return (
-    <section class="bg-indigo-50">
-      <div class="container m-auto max-w-2xl py-24">
+    <section className="bg-indigo-50">
+      <div className="container m-auto max-w-2xl py-24">
         <div
-          class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
+          className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
           <form>
-            <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
-            <div class="mb-4">
-              <label for="type" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label for="type" className="block text-gray-700 font-bold mb-2"
                 >Job Type</label
               >
               <select
                 id="type"
                 name="type"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 required
               >
                 <option value="Full-Time">Full-Time</option>
@@ -27,42 +27,42 @@ const AddJobPage = () => {
               </select>
             </div>
 
-            <div class="mb-4">
-              <label class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2"
                 >Job Listing Name</label
               >
               <input
                 type="text"
                 id="title"
                 name="title"
-                class="border rounded w-full py-2 px-3 mb-2"
+                className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="eg. Beautiful Apartment In Miami"
                 required
               />
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
               <label
                 for="description"
-                class="block text-gray-700 font-bold mb-2"
+                className="block text-gray-700 font-bold mb-2"
                 >Description</label
               >
               <textarea
                 id="description"
                 name="description"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
               ></textarea>
             </div>
 
-            <div class="mb-4">
-              <label for="type" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label for="type" className="block text-gray-700 font-bold mb-2"
                 >Salary</label
               >
               <select
                 id="salary"
                 name="salary"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 required
               >
                 <option value="Under $50K">Under $50K</option>
@@ -79,83 +79,83 @@ const AddJobPage = () => {
               </select>
             </div>
 
-            <div class='mb-4'>
-              <label class='block text-gray-700 font-bold mb-2'>
+            <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
                 Location
               </label>
               <input
                 type='text'
                 id='location'
                 name='location'
-                class='border rounded w-full py-2 px-3 mb-2'
+                className='border rounded w-full py-2 px-3 mb-2'
                 placeholder='Company Location'
                 required           
               />
             </div>
 
-            <h3 class="text-2xl mb-5">Company Info</h3>
+            <h3 className="text-2xl mb-5">Company Info</h3>
 
-            <div class="mb-4">
-              <label for="company" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label for="company" className="block text-gray-700 font-bold mb-2"
                 >Company Name</label
               >
               <input
                 type="text"
                 id="company"
                 name="company"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
               />
             </div>
 
-            <div class="mb-4">
+            <div className="mb-4">
               <label
                 for="company_description"
-                class="block text-gray-700 font-bold mb-2"
+                className="block text-gray-700 font-bold mb-2"
                 >Company Description</label
               >
               <textarea
                 id="company_description"
                 name="company_description"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
               ></textarea>
             </div>
 
-            <div class="mb-4">
+            <div className="mb-4">
               <label
                 for="contact_email"
-                class="block text-gray-700 font-bold mb-2"
+                className="block text-gray-700 font-bold mb-2"
                 >Contact Email</label
               >
               <input
                 type="email"
                 id="contact_email"
                 name="contact_email"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Email address for applicants"
                 required
               />
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
               <label
                 for="contact_phone"
-                class="block text-gray-700 font-bold mb-2"
+                className="block text-gray-700 font-bold mb-2"
                 >Contact Phone</label
               >
               <input
                 type="tel"
                 id="contact_phone"
                 name="contact_phone"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Optional phone for applicants"
               />
             </div>
 
             <div>
               <button
-                class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 Add Job
