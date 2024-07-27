62525d69d954af4e68c1515a0fe3508c044a5d03
DeenoDev <deyanoklaasen@outlook.com>
2024-07-27 23:30:01 +0200

    Moved router script and addJob function into App function in App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
+const addJob = (newJob) => {
+  console.log(newJob);
+}
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +25,24 @@ const router = createBrowserRouter(
 );
 
 const App = () => {
+   
+  const addJob = (newJob) => {
+    console.log(newJob);
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element = {<MainLayout />}>
+      <Route index element={<HomePage/>} />
+      <Route path='/jobs' element={<JobsPage/>} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader} />
+      <Route path='*' element={<NotFoundPage/>} />
+    </Route>
+    )
+  );
+  
+
   return <RouterProvider router={router} />;
 };
 
