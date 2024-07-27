6cae74006d26b3826fe038e39595c7876898c56a
DeenoDev <deyanoklaasen@outlook.com>
2024-07-27 14:31:38 +0200

    created newJob Object within the submit for function.

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -12,13 +12,33 @@ const AddJobPage = () => {
   const [contactEmail, setContactEmail] = useState('');
   const [contactPhone, setContactPhone] = useState('');
 
+  const submitForm = (e) => { 
+    e.preventDefault();
+
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone
+      },
+    }
+
+   }
+   
+
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form>
+          <form onSubmit={submitForm}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
