b41045bf4c547d2230c8face00d5e40440fd03ad
DeenoDev <deyanoklaasen@outlook.com>
2024-07-28 19:56:41 +0200

    imported EditJobPage into App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   //Add New Job
---
+++ src/pages/EditJobPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditJobPage = () => {
+  return (
+    <div>EditJobPage</div>
+  )
+}
+
+export default EditJobPage
