097602b4127a1c1892eb615e8d79989de4767e0a
DeenoDev <deyanoklaasen@outlook.com>
2024-07-28 19:05:55 +0200

    Initialized deleteJob async function and used it as a prop in JobPage route.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
-   
+  //Add New Job
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -20,13 +20,19 @@ const App = () => {
     return;
   };
 
+  //Delete Job
+
+  const deleteJob = async (id) => {
+    console.log('delete', id);
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path='/' element = {<MainLayout />}>
       <Route index element={<HomePage/>} />
       <Route path='/jobs' element={<JobsPage/>} />
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
-      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader} />
+      <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob }/>} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage/>} />
     </Route>
     )
