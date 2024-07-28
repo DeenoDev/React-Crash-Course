974fb66d8bb576679d6c24c6b3616b0eb0881ef0
DeenoDev <deyanoklaasen@outlook.com>
2024-07-28 19:46:09 +0200

    imported ToastContainer from react-toastify.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/Navbar';
-import HomeCards from '../components/HomeCards';
+
 
 const MainLayout = () => {
   return (
