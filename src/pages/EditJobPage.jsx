9ea4c54f11c915d736e53c1cb9defcff3c24482e
DeenoDev <deyanoklaasen@outlook.com>
2024-07-29 22:22:31 +0200

    set useLoaderData to job variable.

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {useParams, useLoaderData, useNavigate} from 'react-router-dom';
 
 const EditJobPage = () => {
+  const job = useLoaderData();
+
   return (
     <div>EditJobPage</div>
   )
